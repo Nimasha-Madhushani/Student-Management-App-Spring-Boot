d1cb5641802c95376be709c03ea775363a82694f
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-18 23:39:20 +0530

    initiate controller to add student

--- src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
+++ src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
@@ -1,4 +1,23 @@
 package com.example.StudentManagementSystem.controller;
 
+import com.example.StudentManagementSystem.model.Student;
+import com.example.StudentManagementSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
 }
--- src/main/java/com/example/StudentManagementSystem/service/StudentService.java
+++ src/main/java/com/example/StudentManagementSystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.example.StudentManagementSystem.service;
 
+import com.example.StudentManagementSystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
