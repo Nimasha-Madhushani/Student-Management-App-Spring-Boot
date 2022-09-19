b7decd51ac84b96b8319824e9ecbbe96e3ab907c
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-19 07:58:48 +0530

    initiate functionality to retrieve students details

--- src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
+++ src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.example.StudentManagementSystem.controller;
 import com.example.StudentManagementSystem.model.Student;
 import com.example.StudentManagementSystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -16,8 +15,14 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/add")
-    public String add(@RequestBody Student student){
+    public String add(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "New student is added";
     }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudent();
+    }
+
 }
--- src/main/java/com/example/StudentManagementSystem/service/StudentService.java
+++ src/main/java/com/example/StudentManagementSystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.example.StudentManagementSystem.service;
 
 import com.example.StudentManagementSystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudent();
 }
