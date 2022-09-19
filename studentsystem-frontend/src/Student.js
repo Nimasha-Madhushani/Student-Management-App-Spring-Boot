7db7646bf551938620576cd3ccd8890d752c04f0
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-19 17:13:25 +0530

    user interface design and function for add students

--- studentsystem-frontend/src/Student.js
+++ studentsystem-frontend/src/Student.js
@@ -1,10 +1,24 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student);
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("Student is added");
+    });
+  };
   return (
     <form>
       <Paper elevation={3} style={paperStyle}>
@@ -24,15 +38,22 @@ export default function Student() {
             label="Name"
             variant="outlined"
             fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <TextField
             id="outlined-basic"
             label="Address"
             variant="outlined"
             fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
         </Box>
       </Paper>
+      <Button variant="contained" onClick={handleClick}>
+        SUBMIT
+      </Button>
     </form>
   );
 }
